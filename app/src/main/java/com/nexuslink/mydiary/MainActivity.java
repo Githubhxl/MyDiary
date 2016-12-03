0905e5b6075b09514c7dd6807e2d01cd2b76dc21
RUI1806415923 <1806415923@qq.com>
2016-12-03 16:17:29 +0800

    Initial commit

---
+++ app/src/main/java/com/nexuslink/mydiary/MainActivity.java
@@ -0,0 +1,26 @@
+package com.nexuslink.mydiary;
+
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setNavigation();
+    }
+
+    private void setNavigation() {
+
+    }
+}
