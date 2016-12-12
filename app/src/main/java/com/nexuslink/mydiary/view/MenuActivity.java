685119e57e7b6ec116685f05002e3632f3f10f23
RUI1806415923 <1806415923@qq.com>
2016-12-12 13:09:14 +0800

    备忘添加，删除修改功能

--- app/src/main/java/com/nexuslink/mydiary/view/MenuActivity.java
+++ app/src/main/java/com/nexuslink/mydiary/view/MenuActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -26,14 +27,24 @@ import com.yanzhenjie.recyclerview.swipe.SwipeMenuCreator;
 import com.yanzhenjie.recyclerview.swipe.SwipeMenuItem;
 import com.yanzhenjie.recyclerview.swipe.SwipeMenuRecyclerView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MenuActivity extends AppCompatActivity implements OnSwipeMenuItemClickListener, OnItemClickListener {
+public class MenuActivity extends AppCompatActivity implements OnSwipeMenuItemClickListener {
     @BindView(R.id.forget)
     SwipeMenuRecyclerView swipeMenuRecyclerView;
 
     private MenuAdapter menuAdapter;
+    private AlertView deleteAlert;
+    private AlertView editAlert;
+    private AlertView addAlert;
+
+    private List<String> list = new ArrayList<>();
+    private EditText editText;
 
     private static final int TYPECALL = 1;
     private static final int TYPEDIARY = 2;
@@ -73,6 +84,8 @@ public class MenuActivity extends AppCompatActivity implements OnSwipeMenuItemCl
         setContentView(R.layout.activity_menu);
         ButterKnife.bind(this);
         initRecyclerView();
+        list.add("sdkl");
+        list.add("sdk2");list.add("sdk3");list.add("sdk4");list.add("sdkl");list.add("sdk5");list.add("sdk58");
     }
 
     private void initRecyclerView() {
@@ -81,7 +94,7 @@ public class MenuActivity extends AppCompatActivity implements OnSwipeMenuItemCl
         swipeMenuRecyclerView.setItemAnimator(new DefaultItemAnimator());
         swipeMenuRecyclerView.setSwipeMenuCreator(swipeMenuCreator);
         swipeMenuRecyclerView.setSwipeMenuItemClickListener(MenuActivity.this);
-        swipeMenuRecyclerView.setAdapter( menuAdapter = new MenuAdapter());
+        swipeMenuRecyclerView.setAdapter( menuAdapter = new MenuAdapter(list));
         menuAdapter.setOnItemClickListener(new NormalViewHolder.OnItemClickListener() {
             @Override
             public void onClick(RecyclerView.ViewHolder holder, int position) {
@@ -89,15 +102,13 @@ public class MenuActivity extends AppCompatActivity implements OnSwipeMenuItemCl
                     Intent intent = new Intent(MenuActivity.this,EditActivityForget.class);
                     startActivity(intent);
                     Toast.makeText(MenuActivity.this,position+"",Toast.LENGTH_SHORT).show();
-
-
             }
         });
     }
 
 
     @Override
-    public void onItemClick(Closeable closeable, int adapterPosition, int menuPosition, int direction) {
+    public void onItemClick(Closeable closeable, final int adapterPosition, int menuPosition, int direction) {
         closeable.smoothCloseMenu();
         /*Toast.makeText(MenuActivity.this, "list第" + adapterPosition + "; 右侧菜单第" + menuPosition, Toast.LENGTH_SHORT).show();
         if(adapterPosition == 0){
@@ -110,31 +121,61 @@ public class MenuActivity extends AppCompatActivity implements OnSwipeMenuItemCl
         if(direction == SwipeMenuRecyclerView.RIGHT_DIRECTION){
             switch (menuPosition){
                 case 0:
-                    new AlertView("提示", "确认删除备忘？", "取消", null, new String[]{"确认"}, this,
-                            AlertView.Style.Alert, this)
-                            .show();
+                    deleteAlert = new AlertView("提示", "确认删除备忘？", "取消", null, new String[]{"确认"}, this,
+                            AlertView.Style.Alert, new OnItemClickListener() {
+                        @Override
+                        public void onItemClick(Object o, int position) {
+                            if(position == 0){
+                                menuAdapter.deleteItem(adapterPosition);
+                            }
+
+                        }
+                    });
+                    deleteAlert.show();
                     break;
                 case 1:
-                    new AlertView("编辑备忘", null, "取消", null, new String[]{"完成"}, this,
-                            AlertView.Style.Alert, this).addExtView(LayoutInflater.from(this).inflate(R.layout.dialog_additem,null))
-                            .show();
+                    View view = LayoutInflater.from(this).inflate(R.layout.dialog_additem,null);
+                    final EditText edit = (EditText) view.findViewById(R.id.edit_item);
+                    edit.setText(list.get(adapterPosition - 2));
+                    editAlert = new AlertView("编辑备忘", null, "取消", null, new String[]{"完成"}, this,
+                            AlertView.Style.Alert, new OnItemClickListener() {
+                        @Override
+                        public void onItemClick(Object o, int position) {
+                            if (position == 0){
+                                menuAdapter.changeItem(edit.getText().toString(),adapterPosition);
+                            }
+                        }
+                    }).addExtView(view);
+                    editAlert.show();
+
                     break;
             }
         }
 
     }
 
-    @Override
-    public void onItemClick(Object o, int position) {
-        switch (position){
-            case 1:
-
-                break;
-        }
-    }
-
     private class MenuAdapter extends SwipeMenuAdapter<RecyclerView.ViewHolder> {
+        private List<String> list;
         private NormalViewHolder.OnItemClickListener onItemClickListener;
+
+        public MenuAdapter(List<String> list) {
+            this.list = list;
+        }
+
+        public void deleteItem(int position){
+            list.remove(position - 2);
+            notifyItemRemoved(position);
+        }
+        public void additem(String s) {
+            list.add(0,s);
+            notifyItemInserted(2);
+        }
+
+        public void changeItem(String s, int adapterPosition) {
+            list.set(adapterPosition - 2,s);
+            notifyDataSetChanged();
+        }
+
         @Override
         public View onCreateContentView(ViewGroup parent, int viewType) {
             View view = LayoutInflater.from(MenuActivity.this).inflate(R.layout.item_forget,parent,false);
@@ -173,6 +214,7 @@ public class MenuActivity extends AppCompatActivity implements OnSwipeMenuItemCl
         @Override
         public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
             if( holder instanceof NormalViewHolder){
+                ((NormalViewHolder) holder).textView.setText(list.get(position - 2));
                 ((NormalViewHolder) holder).rootView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -184,17 +226,18 @@ public class MenuActivity extends AppCompatActivity implements OnSwipeMenuItemCl
 
         @Override
         public int getItemCount() {
-            return 6;
+            return list.size()+ 3;
         }
     }
 
     //三种viewHolder
     private static class NormalViewHolder extends RecyclerView.ViewHolder {
         private LinearLayout rootView;
+        private TextView textView;
         public NormalViewHolder(View itemView) {
             super(itemView);
             rootView = (LinearLayout) itemView.findViewById(R.id.rootview_forget);
-            TextView textView = (TextView) itemView.findViewById(R.id.forget_title);
+            textView = (TextView) itemView.findViewById(R.id.forget_title);
             textView.setText("备忘事项1");
         }
         public interface OnItemClickListener{
@@ -243,15 +286,25 @@ public class MenuActivity extends AppCompatActivity implements OnSwipeMenuItemCl
 
     private class AddForgetHolder extends RecyclerView.ViewHolder {
         private ImageView imageView;
+        private View editView;
         public AddForgetHolder(View view) {
             super(view);
             imageView  = (ImageView) view.findViewById(R.id.add_forget_items);
             imageView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    new AlertView("添加", null, "取消", null, new String[]{"完成"}, MenuActivity.this,
-                            AlertView.Style.Alert, MenuActivity.this).addExtView(LayoutInflater.from(MenuActivity.this).inflate(R.layout.dialog_additem,null))
-                            .show();
+                     addAlert = new AlertView("添加", null, "取消", null, new String[]{"完成"}, MenuActivity.this,
+                            AlertView.Style.Alert, new OnItemClickListener() {
+                        @Override
+                        public void onItemClick(Object o, int position) {
+                            if (position == 0){
+                                EditText edit = (EditText) editView.findViewById(R.id.edit_item);
+                                menuAdapter.additem(edit.getText().toString());
+                            }
+
+                        }
+                    }).addExtView(editView = LayoutInflater.from(MenuActivity.this).inflate(R.layout.dialog_additem,null));
+                    addAlert.show();
                 }
             });
         }
