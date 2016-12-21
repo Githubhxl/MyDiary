73fb2a874cb0c77c66057bca48c3dde33532227d
RUI1806415923 <1806415923@qq.com>
2016-12-21 21:27:32 +0800

    添加启动页和图标

--- app/src/main/java/com/nexuslink/mydiary/view/WelcomeActivity.java
+++ app/src/main/java/com/nexuslink/mydiary/view/WelcomeActivity.java
@@ -1,5 +1,7 @@
 package com.nexuslink.mydiary.view;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +13,14 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },800);
+
     }
 }
